6d1e86cf6275db639dc032e94e1dc7b8d2765217
omkark1558-byte <omkark1558@gmail.com>
2025-12-26 00:30:06 +0530

    Logical

--- src/hash_map/Logical_1.java
+++ src/hash_map/Logical_1.java
@@ -1,26 +1,31 @@
 package hash_map;
- import java.util.*;
+
+import java.util.*;
 
 public class Logical_1 {
 
 	public static void main(String[] args) {
-		 
-		
+
 		String name = "OOMKAAKRRTTKARO";
-		
-		char[] ch = name.toCharArray();
-		
-		Map<Character,Integer> map = new HashMap<Character,Integer>();
-		
-		for(char  h : ch)
-		{
-			 int frequency = 1;
-			 if(map.containsKey(h))
-			 {
-				 frequency=map.get(h)+1;
-			 }
-			 map.put(h, frequency);
+
+//		char[] ch = name.toCharArray();
+
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+
+		for (char ch : name.toCharArray()) {
+			map.put(ch, map.getOrDefault(ch, 0) + 1);
 		}
+
 		System.out.println(map);
+//		for(char  h : ch)
+//		{
+//			 int frequency = 1;
+//			 if(map.containsKey(h))
+//			 {
+//				 frequency=map.get(h)+1;
+//			 }
+//			 map.put(h, frequency);
+//		}
+//		System.out.println(map);
 	}
 }
