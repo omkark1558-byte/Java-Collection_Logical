384f8b20266f4bdd52f6473ff925dba91fb191f3
omkark1558-byte <omkark1558@gmail.com>
2026-01-05 22:55:09 +0530

    Logical List

---
+++ src/hash_map/Logical_4.java
@@ -0,0 +1,15 @@
+package hash_map;
+
+import java.util.*;
+public class Logical_4 {
+	public static void main (String [] args)
+	{
+	
+		Map<Integer,Character> map = new HashMap<>();
+		map.put(1, 'A');
+		map.put(2, 'A');
+		map.put(3, 'A');
+		map.put(4, 'A');
+		System.out.println(map);
+	}
+}
